4fa535bc5c11b79440e23490356397979ffc6a2a
Dineshpandi1320 <dineshd1307@gmail.com>
2023-08-24 15:38:28 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+const Footer = () => {
+  return (
+    <>
+      <footer className="sticky-footer bg-white">
+        <div className="container my-auto">
+          <div className="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2021</span>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
